6135b940542d573057b0f4fd3ea1bcc2551fe5cb
Spencer Smith <spencer.t.smith-1@ou.edu>
2023-09-15 11:14:43 -0500

    Main completed

--- gitassignment.py
+++ gitassignment.py
@@ -1,8 +1,17 @@
 def sumlist(list):
     return sum(list)
+
 def productlist(list):
     product = 1
-    for i in range(list):
-        product *= i
-        
+    for i in list:
+        product = product * i
+    return product
+
+input = input()
+list = input.split()
+list = [int(x) for x in list]
+
+print(sumlist(list))
+print(productlist(list))
+
 
